cf9a8e59c47b669cd7a23620e2aad89eaab2de4f
Tony <123567427+rizaton@users.noreply.github.com>
2023-11-06 13:14:33 +0700

    Add
    Adding Home Page

--- lib/ui/homePage/home_page.dart
+++ lib/ui/homePage/home_page.dart
@@ -1,208 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:gubuk_cinema/ui/loginPage/login_page.dart';
+import 'package:gubuk_cinema/ui/loginPage/registration_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    width: 360,
-    height: 800,
-    clipBehavior: Clip.antiAlias,
-    decoration: const BoxDecoration(color: Colors.white),
-    child: Stack(
-        children: [
-            Positioned(
-                left: 0,
-                top: 765,
-                child: Container(
-                    width: 360,
-                    height: 35,
-                    decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            backgroundColor: Colors.black,
+            expandedHeight: 70.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Gubuk Cinema',
+                style: TextStyle(
+                  color: Colors.grey,
                 ),
+              ),
+              centerTitle: true,
             ),
-            Positioned(
-                left: 0,
-                top: 80,
-                child: SizedBox(
-                    width: 360,
-                    height: 50,
+          ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return SizedBox(
+                  height: 400,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailPage(index),
+                        ),
+                      );
+                    },
                     child: Stack(
-                        children: [
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                    width: 360,
-                                    height: 50,
-                                    decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
-                                ),
+                      children: [
+                        Container(
+                          height: double.infinity,
+                          width: double.infinity,
+                          color: Colors.blue,
+                          child: Image.asset(
+                            '../lib/assets/indigo.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Container(
+                            color: Colors.white,
+                            child: ListTile(
+                              title: Text('Deskripsi Item $index',
+                                  style: const TextStyle(color: Colors.black)),
+                              subtitle: const Text('Rating: 4.5',
+                                  style: TextStyle(color: Colors.black)),
                             ),
-                            Positioned(
-                                left: 25,
-                                top: 4,
-                                child: Container(
-                                    width: 310,
-                                    height: 40,
-                                    decoration: const BoxDecoration(color: Color(0xFF9A8383)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 298,
-                                top: 11,
-                                child: Container(
-                                    width: 30,
-                                    height: 28,
-                                    decoration: const BoxDecoration(color: Color(0xFF747474)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 30,
-                                top: 11,
-                                child: Container(
-                                    width: 260,
-                                    height: 28,
-                                    decoration: const BoxDecoration(color: Colors.white),
-                                ),
-                            ),
-                        ],
+                          ),
+                        ),
+                      ],
                     ),
-                ),
+                  ),
+                );
+              },
+              childCount: 20,
             ),
-            Positioned(
-                left: 10,
-                top: 140,
-                child: SizedBox(
-                    width: 340,
-                    height: 620,
-                    child: Stack(
-                        children: [
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                    width: 340,
-                                    height: 620,
-                                    decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 20,
-                                top: 20,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 190,
-                                top: 20,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 20,
-                                top: 170,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 190,
-                                top: 170,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 20,
-                                top: 320,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 190,
-                                top: 320,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 20,
-                                top: 470,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 190,
-                                top: 470,
-                                child: Container(
-                                    width: 130,
-                                    height: 130,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                        ],
-                    ),
-                ),
-            ),
-            Positioned(
-                left: 0,
-                top: 0,
-                child: Container(
-                    width: 360,
-                    height: 70,
-                    child: Stack(
-                        children: [
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                    width: 360,
-                                    height: 70,
-                                    decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 115,
-                                top: 15,
-                                child: Container(
-                                    width: 165,
-                                    height: 40,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                            Positioned(
-                                left: 20,
-                                top: 15,
-                                child: Container(
-                                    width: 40,
-                                    height: 40,
-                                    decoration: const BoxDecoration(color: Color(0xFF636363)),
-                                ),
-                            ),
-                        ],
-                    ),
-                ),
-            ),
+          ),
         ],
-    ),
-);
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text("Log in"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Register"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Registration(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Tentang Aplikasi"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class DetailPage extends StatelessWidget {
+  final int index;
+
+  const DetailPage(this.index, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Detail Item $index'),
+
+      ),
+      body: Center(
+        child: Text('Detail dari item $index'),
+      ),
+    );
+  }
+}
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: ListView(
+  //       scrollDirection: Axis.vertical,
+  //       children: [
+  //         Column(
+  //           children: [
+  //             SizedBox(
+  //               width: double.infinity,
+  //               child: Stack(
+  //                 children: [
+  //                   Container(
+  //                     width: double.infinity,
+  //                     height: 70,
+  //                     color: const Color.fromARGB(255, 0, 0, 0),
+  //                     child: Stack(
+  //                       children: [
+  //                         Align(
+  //                           alignment: Alignment.center,
+  //                           child: Container(
+  //                             width: 165,
+  //                             height: 40,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 170, 0, 0)),
+  //                           ),
+  //                         ),
+  //                         Positioned(
+  //                           top: 15,
+  //                           left: 20,
+  //                           child: Container(
+  //                             width: 40,
+  //                             height: 40,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 180, 112, 112)),
+  //                           ),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //             SizedBox(
+  //               width: double.infinity,
+  //               child: Stack(
+  //                 children: [
+  //                   Container(
+  //                     width: double.infinity,
+  //                     height: 50,
+  //                     decoration: const BoxDecoration(color: Color(0xFFD9D9D9)),
+  //                     child: Stack(
+  //                       children: [
+  //                         Align(
+  //                           alignment: Alignment.center,
+  //                           child: Container(
+  //                             width: 300,
+  //                             height: 40,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 116, 116, 116)),
+  //                           ),
+  //                         ),
+  //                         Align(
+  //                           alignment: Alignment.center,
+  //                           child: Container(
+  //                             width: 290,
+  //                             height: 30,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 185, 185, 185)),
+  //                           ),
+  //                         ),
+  //                         Positioned(
+  //                           top: 10,
+  //                           left: 310,
+  //                           child: Container(
+  //                             width: 30,
+  //                             height: 30,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 101, 100, 100)),
+  //                           ),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //             SizedBox(
+  //               width: double.infinity,
+  //               child: Stack(
+  //                 children: [
+  //                   Container(
+  //                     width: double.infinity,
+  //                     height: 70,
+  //                     color: const Color.fromARGB(255, 116, 116, 116),
+  //                     child: Stack(
+  //                       children: [
+  //                         Positioned(
+  //                           top: 15,
+  //                           left: 20,
+  //                           child: Container(
+  //                             width: 40,
+  //                             height: 40,
+  //                             decoration: const BoxDecoration(
+  //                                 color: Color.fromARGB(255, 145, 144, 144)),
+  //                           ),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+// }
