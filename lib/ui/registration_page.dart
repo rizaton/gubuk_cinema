49957ee67435c05773865dfdf56beffd5eb1aa31
FikalAhmad <fikaalm26@gmail.com>
2023-11-09 19:49:02 +0700

    chore(registration_page): change register page

--- lib/ui/registration_page.dart
+++ lib/ui/registration_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:gubuk_cinema/models/http_api.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -8,12 +10,38 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController fullnameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController reenterPasswordController =
+      TextEditingController();
+
+  Future<void> registerUser() async {
+    final String username = usernameController.text;
+    final String fullname = fullnameController.text;
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    final Map<String, dynamic> userData = {
+      'username': username,
+      'fullname': fullname,
+      'email': email,
+      'password': password,
+    };
+
+    final String jsonData = jsonEncode(userData);
+
+    postAPIAccount(jsonData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         appBar: AppBar(
-          title: Text("Register"),
-          backgroundColor: Colors.blue,
+          title: Text('Halaman Pendaftaran'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(50),
@@ -21,7 +49,7 @@ class _RegisterState extends State<Register> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
-                'Register',
+                'Daftar Akun',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
@@ -34,20 +62,25 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 16),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Username'),
+                controller: usernameController,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Nama'),
+                controller: fullnameController,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Email'),
+                controller: emailController,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true, // Untuk menyembunyikan teks
+                obscureText: true,
+                controller: passwordController,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Ulangi Password'),
                 obscureText: true,
+                controller: reenterPasswordController,
               ),
               SizedBox(height: 16),
               Row(
@@ -59,12 +92,14 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
-                  // Aksi yang akan diambil ketika tombol ditekan (misalnya, mendaftar pengguna)
+                  registerUser();
                 },
                 child: Text('Daftar'),
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
