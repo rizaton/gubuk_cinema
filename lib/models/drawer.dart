015989310b1e68ca92f01181d80af0d7da6e75de
FikalAhmad <fikaalm26@gmail.com>
2023-11-10 19:40:37 +0700

    chore(drawer): change drawer file

--- lib/models/drawer.dart
+++ lib/models/drawer.dart
@@ -5,10 +5,7 @@ import 'package:gubuk_cinema/ui/registration_page.dart';
 class DrawerSide extends StatefulWidget {
   final String loggedOn;
 
-  const DrawerSide({
-    super.key,
-    required this.loggedOn
-    });
+  const DrawerSide({super.key, required this.loggedOn});
 
   @override
   State<DrawerSide> createState() => _DrawerSideState();
@@ -43,8 +40,9 @@ class _DrawerNoLogged extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                Image.asset('lib/assets/gubukcinemalogo.png',
-                    ),
+                Image.asset(
+                  'lib/assets/gubukcinemalogo.png',
+                ),
                 const SizedBox(
                   height: 20,
                 ),
@@ -55,10 +53,9 @@ class _DrawerNoLogged extends StatelessWidget {
             leading: const Icon(Icons.login),
             title: const Text('Login'),
             onTap: () {
-              Navigator.push(context, 
-                MaterialPageRoute(builder: 
-                (context) => const LoginPage()
-                ),
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
               );
             },
           ),
@@ -66,11 +63,8 @@ class _DrawerNoLogged extends StatelessWidget {
             leading: const Icon(Icons.app_registration),
             title: const Text('Register'),
             onTap: () {
-              Navigator.push(context, 
-                MaterialPageRoute(builder: 
-                (context) => const Register()
-                )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Register()));
             },
           ),
           ListTile(
@@ -85,6 +79,69 @@ class _DrawerNoLogged extends StatelessWidget {
             title: Text('Logout'),
             onTap: () {
               // Handle Logout action
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DrawerLogged extends StatelessWidget {
+  const _DrawerLogged();
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Image.asset(
+                  'lib/assets/gubukcinemalogo.png',
+                ),
+                Text('Welcome KAL', style: TextStyle(color: Colors.white)),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('About Application'),
+            onTap: () {
+              // Handle About Application action
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.bookmark),
+            title: const Text('Bookmark'),
+            onTap: () {
+              // Handle Bookmark action
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('User Settings'),
+            onTap: () {
+              // Handle User Settings action
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              // String loggedOut = '1';
             },
           ),
         ],
